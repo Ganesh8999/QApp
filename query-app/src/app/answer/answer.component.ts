9ba3cf8007e129721b55aabd776b0b91b3014fb6
Ganesh Singh <singhganesh571@gmail.com>
2019-06-29 09:15:43 +0530

    QuestionCount
    Added question Counter and ngFor for cards

--- query-app/src/app/answer/answer.component.ts
+++ query-app/src/app/answer/answer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-answer',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./answer.component.css']
 })
 export class AnswerComponent implements OnInit {
-
-  constructor() { }
+  resp: any;
+  questionCount: number;
+  public show:boolean = false;
+ public  c:number;
+  constructor(private httpobj: HttpClient) { }
 
   ngOnInit() {
+    let responseData=this.httpobj.get("./assets/queryContent.json");
+    responseData.subscribe((response)=>{
+      this.resp=response; console.log(this.resp);
+      this.questionCount = this.resp.length;
+     this.c = 0;
+    });
+  }
+
+  openAnswerWindow() {
+    this.show = !this.show;
   }
 
 }
